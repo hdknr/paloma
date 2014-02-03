d531a8ae5632bd5babba879ccb00ae4e695dd7d7
hdknr <gmail@hdknr.com>
2014-02-03 10:27:39 +0900

    Cleanup sample project

--- example/app/settings.py
+++ example/app/settings.py
@@ -1,8 +1,5 @@
-# Django settings for example project.
-import sys,os
-#
-PROJECT_DIR=os.path.dirname( os.path.abspath(__file__))
-#
+# Django settings for app project.
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,46 +9,31 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#: Postfix master.cf doesn't seems to load environment variables
-#:
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': os.environ.get('PALOMA_DEFAULT_DBNAME','paloma'),
-#        'USER': os.environ.get('PALOMA_DEFAULT_DBUSER','paloma'),
-#        'PASSWORD':os.environ.get('PALOMA_DEFAULT_DBPASSWORD','paloma'),
-#        'HOST': os.environ.get('PALOMA_DEFAULT_DBHOST','localhost'),
-        'NAME': "paloma_dev",
-        'USER': "paloma_dev",
-        'PASSWORD':"paloma_dev",
-        'HOST': "localhost",
-        'PORT': '',                     
-        'TEST_CHARSET': 'utf8',
-        'TEST_DATABASE_COLLATION': 'utf8_general_ci',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
-#    #:- this secondary database is for postfix-mysql 
-#    ,'postfix': {
-#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'postfix',                      # Or path to database file if using sqlite3.
-#        'USER': 'postfix',                      # Not used with sqlite3.
-#        'PASSWORD': 'postfix',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
 }
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Asia/Tokyo'
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'ja-jp'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -67,22 +49,22 @@ USE_L10N = True
 USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+# Example: "/var/www/example.com/media/"
 MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+# Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
+# Example: "/var/www/example.com/static/"
 STATIC_ROOT = ''
 
 # URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+# Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
@@ -90,12 +72,9 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR,'static'),
-
 )
 
 # List of finder classes that know how to find static files in
-
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -104,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '%p!7=*6v2ldki!z(wu0@gr--g_f!6*vh)-+imd7cf29o$gt2^h'
+SECRET_KEY = 'z!9@tw#2@s%g$xz_$-*mt%^gl6so88^nzj+aq(7&s7%t_5wq#+'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -129,7 +108,9 @@ ROOT_URLCONF = 'app.urls'
 WSGI_APPLICATION = 'app.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR,'templates'),      # html
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
@@ -140,7 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -173,75 +154,7 @@ LOGGING = {
         },
     }
 }
-
-######## Custom Configuration 
-
-# - south for data migration
-if 'test' not in sys.argv:
-    #: Use south after celery related tables are created.
-    INSTALLED_APPS +=('south',)  #: for Model Migration
-    pass
-
-# - django-celery for asynchoronous task queue
 #
-INSTALLED_APPS += ('djcelery','kombu.transport.django',)
-#: Broker
-#BROKER_URL="django://"
-#BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-BROKER_URL = 'amqp://paloma:paloma@localhost:5672/paloma'
-#BROKER_URL = 'redis://localhost:6379/0'
-#BROKER_URL = 'mongodb://localhost:27017/paloma'
-#
-#CELERY_ALWAYS_EAGER = True  #:True: synchronous
-#: Serializer
-#CELERY_TASK_SERIALIZER='json'
-CELERY_TASK_SERIALIZER='pickle'
-#: Persistent Worker State
-CELERYD_STATE_DB=os.path.join(PROJECT_DIR,'celery_state.db')
-#
-import djcelery
-djcelery.setup_loader()
-
-#CELERY_EMAIL_TASK_CONFIG = {
-#    'queue' : 'django_email',
-#    'delivery_mode' : 1, # non persistent
-#    'rate_limit' : '50/m', # 50 emails per minute
-#}
-
-# --- Paloma Configuration
-#
-INSTALLED_APPS +=('paloma',)  #: this project.
-#
-PALOMA_NAME="mypaloma"          #: name for project
-SMTP_EMAIL_BACKEND='paloma.backends.SmtpEmailBackend' 
-#
-from kombu import Exchange, Queue
-CELERY_DEFAULT_QUEUE = PALOMA_NAME
-CELERY_QUEUES = ( 
-    Queue(PALOMA_NAME, Exchange( PALOMA_NAME ), routing_key=PALOMA_NAME),
+INSTALLED_APPS += (
+    'shops',
 )
-
-# - paloma for mail transfer agents
-if 'test' not in sys.argv:
-    EMAIL_BACKEND = 'paloma.backends.PalomaEmailBackend'
-#    EMAIL_BACKEND = 'paloma.backends.JournalEmailBackend'
-else:
-    #: in testing. save messages directory to Journal model
-    EMAIL_BACKEND = 'paloma.backends.JournalEmailBackend'
-
- 
-# -- sample app
-
-INSTALLED_APPS +=(
-        'app.accounts', #: Sample Membership Management
-        'app.foods',    #:  Sammple application for Paloma
-        ) 
-
-# -- django-extensinon
-
-INSTALLED_APPS +=('django_extensions',)  #:  tools for django-extensions
-
-# - logging
-import applogs
-applogs.config(LOGGING)
-
