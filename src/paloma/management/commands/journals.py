08d5079cf07ca8aeb50018102f4c86874b2d814c
hdknr <gmail@hdknr.com>
2014-02-03 18:02:46 +0900

    journals process --id=32 command PEP8

--- src/paloma/management/commands/journals.py
+++ src/paloma/management/commands/journals.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings 
-from django.contrib.auth.models import User 
-
-from optparse import make_option
-from datetime import datetime
-import commands
-import os
-
 from . import GenericCommand
 from ...models import Journal
 from ...tasks import process_journal
@@ -16,22 +8,23 @@ class Command(GenericCommand):
     ''' paloma postfix management
     '''
 
-    option_list = GenericCommand.option_list + (
-        )
+    option_list = GenericCommand.option_list + ()
     ''' Command Option '''
 
-    def handle_process(self,journal_id,*args,**options):
+    def handle_process(self, id, *args, **options):
         '''　process journals
         '''
-        
+
         try:
-            process_journal(journal_id)
-        except Journal.DoesNotExist,e:
-            print journal_id , "was not found"
+            process_journal(id)
 
-        except Exception,e:
-            print "Error:",e
+        except Journal.DoesNotExist:
+            print "Journal id=", id, "was not found"
 
-    def handle_list(self,count=10,*args,**options):
-        for j in Journal.objects.order_by('-id'): 
-            print j.id ,j.dt_created,j.sender, j.recipient, "Jailed" if j.is_jailed else ""
+        except Exception, e:
+            print "Error:", e
+
+    def handle_list(self, count=10, *args, **options):
+        for j in Journal.objects.order_by('-id'):
+            if j.is_jailed:
+                print j.id, j.dt_created, j.sender, j.recipient, "Jailed"
