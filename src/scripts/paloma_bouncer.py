de6a2373fbbf7f13ab209c3d0f38f2c6145ad40d
hidelafoglia <gmail@hdknr.com>
2014-08-07 17:16:16 +0900

    Django 1.6

--- src/scripts/paloma_bouncer.py
+++ src/scripts/paloma_bouncer.py
@@ -24,7 +24,6 @@ if __name__ == '__main__':
     #: argv[1] is manage.py command 
     sys.argv[1] = 'bouncer'
 
-    from django.core.management import execute_manager
     import imp
     try:
         imp.find_module('settings') # Assumed to be in the same directory.
@@ -33,11 +32,7 @@ if __name__ == '__main__':
         sys.stderr.write(str(sys.path))
         sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
         sys.exit(1)
-    
-    import settings
-    
-    try:
-        print "Executing...", settings.__file__,sys.argv
-        execute_manager(settings)
-    except:
-        print traceback.format_exc() 
+
+    import settings 
+    from django.core.management import execute_from_command_line 
+    execute_from_command_line(sys.argv)
