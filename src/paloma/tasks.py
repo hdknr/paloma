c84aa7ac171355100f00996fa5fabdda79ee1a96
hidelafoglia <gmail@hdknr.com>
2015-08-07 06:04:38 +0900

    time.sleep

--- src/paloma/tasks.py
+++ src/paloma/tasks.py
@@ -4,6 +4,7 @@ from django.core.mail import get_connection
 from django.utils.timezone import now
 from django.db.models.loading import get_model as django_get_model
 from django.utils.translation import ugettext_lazy as _
+import time
 
 
 def get_model(p):
@@ -277,6 +278,7 @@ def enqueue_mails_for_publish(
         for circle in publish.circles.all():
             for member in circle.member_set.filter(
                     **member_filter).exclude(default_ban):
+                time.sleep(20.0/1000.0)
                 pub = Publication.objects.publish(
                     publish, circle, member, signature)
 
